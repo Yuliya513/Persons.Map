fdd09190214f2db51547536204f59b9d36ded898
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-06-18 16:58:46 +0300

    Initial commit

---
+++ src/com/company/Persons.Map.java
@@ -0,0 +1,26 @@
+package com.company;
+
+class HashMap implements Mappublic class Main{
+    public static void main (String [] args)
+    {
+        Map <Integer, String> persons=new HashMap<Integer, String>();
+        persons.put(102053, "Y.S");
+        persons.put(702053, "Y.D");
+        persons.put(7095, "D.D");
+        persons.put(709581, "D.V");
+        persons.put(8179, "Y1.D");
+        persons.put(3000, "D1.D1");
+        persons.put(7555, "P.S");
+        persons.put(1853, "P.D");
+        persons.put(98075, "D2.D");
+        System.out.println(102053, "Y.S");
+        System.out.println(persons.get (3));
+        System.out.println(persons.keySet());
+        System.out.println(persons.value());
+        System.out.println(persons.replace (81791053, "D.D"));
+        System.out.println(persons.get (3008179));
+        System.out.println(persons.remove(key 456841));
+        System.out.println(persons.size);
+        persons.clear();
+    }
+}
